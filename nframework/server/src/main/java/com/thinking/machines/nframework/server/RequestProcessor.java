393e9c866246dd74a41d319d0f842cf6c213f36e
developer-shahrukh <shahrukh4854@gmail.com>
2024-06-20 00:13:24 +0530

    Network Framework Initial commit

---
+++ nframework/server/src/main/java/com/thinking/machines/nframework/server/RequestProcessor.java
@@ -0,0 +1,168 @@
+package com.thinking.machines.nframework.server;
+import java.io.*;
+import java.net.*;
+import com.thinking.machines.nframework.common.*;
+import java.nio.charset.*;
+import java.lang.reflect.*;
+class RequestProcessor extends Thread
+{
+private NFrameworkServer server;
+private Socket socket;
+RequestProcessor(NFrameworkServer server,Socket socket)
+{
+this.server=server;
+this.socket=socket;
+start();
+}
+public void run()
+{
+try
+{
+InputStream is=socket.getInputStream();
+OutputStream os=socket.getOutputStream();
+int bytesToReceive=1024;
+byte tmp[]=new byte[1024];
+byte header[]=new byte[1024];
+int bytesReadCount;
+int i,j,k;
+i=0;
+j=0;
+while(j<bytesToReceive)
+{
+bytesReadCount=is.read(tmp);
+if(bytesReadCount==-1) continue;
+for(k=0;k<bytesReadCount;k++)
+{
+header[i]=tmp[k];
+i++;
+}
+j=j+bytesReadCount;
+}
+int requestLength=0;
+i=1;
+j=1023;
+while(j>=0)
+{
+requestLength=requestLength+(header[j]*i);
+i=i*10;
+j--;
+}
+byte ack[]=new byte[1];
+ack[0]=1;
+os.write(ack,0,1);
+os.flush();
+byte request[]=new byte[requestLength];
+bytesToReceive=requestLength;
+i=0;
+j=0;
+while(j<bytesToReceive)
+{
+bytesReadCount=is.read(tmp);
+if(bytesReadCount==-1) continue;
+for(k=0;k<bytesReadCount;k++)
+{
+request[i]=tmp[k];
+i++;
+}
+j=j+bytesReadCount;
+}
+String requestJSONString=new String(request,StandardCharsets.UTF_8);
+Request requestObject=JSONUtil.fromJSON(requestJSONString,Request.class);
+// the request object contains sevicePath and arguments
+// we want the reference of the TCPService that contains the 
+// Class ref and Method ref
+
+String servicePath=requestObject.getServicePath();
+TCPService tcpService=this.server.getTCPService(servicePath);
+Response responseObject=new Response();
+if(tcpService==null)
+{
+responseObject.setSuccess(false);
+responseObject.setResult(null);
+responseObject.setException(new RuntimeException("Invalid path : "+servicePath));
+}
+else
+{
+Class c=tcpService.c;
+Method method=tcpService.method;
+try
+{
+System.out.println("No problem 3333");
+System.out.println(c.newInstance());
+Object serviceObject=c.newInstance();
+System.out.println(serviceObject);
+
+Object result=method.invoke(serviceObject,requestObject.getArguments());
+System.out.println(result);
+
+responseObject.setSuccess(true);
+responseObject.setResult(result);
+responseObject.setException(null);
+}catch(InstantiationException instantiationException)
+{
+responseObject.setSuccess(false);
+responseObject.setResult(null);
+responseObject.setException(new RuntimeException("Unable to create object of service class associated with the path : "+servicePath));
+System.out.println("*********InstantiationException here**********");
+}
+catch(IllegalAccessException illegalAccessException)
+{
+responseObject.setSuccess(false);
+responseObject.setResult(null);
+responseObject.setException(new RuntimeException("Unable to create object of service class associated with the path : "+servicePath));
+System.out.println("************IllegalAccessException here************");
+}
+catch(InvocationTargetException invocationTargetException)
+{
+Throwable t=invocationTargetException.getCause();
+responseObject.setSuccess(false);
+responseObject.setResult(null);
+responseObject.setException(t);
+System.out.println("***********InvocationTargetException here server side*************");
+}
+}
+String responseJSONString=JSONUtil.toJSON(responseObject);
+byte objectBytes[] =responseJSONString.getBytes(StandardCharsets.UTF_8);
+int responseLength=objectBytes.length;
+int x;
+i=1023;
+x=responseLength;
+header=new byte[1024];
+while(x>0)
+{
+header[i]=(byte)(x%10);
+x=x/10;
+i--;
+}
+os.write(header,0,1024); // from which index, how many
+os.flush();
+while(true)
+{
+bytesReadCount=is.read(ack);
+if(bytesReadCount==-1) continue;
+break;
+}
+int bytesToSend=responseLength;
+int chunkSize=1024;
+j=0;
+while(j<bytesToSend)
+{
+if((bytesToSend-j)<chunkSize) chunkSize=bytesToSend-j;
+os.write(objectBytes,j,chunkSize);
+os.flush();
+j=j+chunkSize;
+}
+while(true)
+{
+bytesReadCount=is.read(ack);
+if(bytesReadCount==-1) continue;
+break;
+}
+socket.close();
+}catch(IOException e)
+{
+System.out.println(e);
+System.out.println("************Other exception in response class*********");
+}
+}
+}
